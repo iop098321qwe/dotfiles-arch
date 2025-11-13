5bab009859990b0c8a7b341679655930ed7a7257
Dallas Elliott <iop098321qwe@pm.me>
2025-11-13 12:19:05 -0700

    feat(script): add proton mail bridge install script

---
+++ .chezmoiscripts/run_onchange_proton_mail_bridge.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install Proton Mail Bridge
+if ! yay -Qs protonmail-bridge &> /dev/null; then
+  yay -S --noconfirm --needed protonmail-bridge
+else
+  echo "protonmail-bridge already installed."
+fi
+
