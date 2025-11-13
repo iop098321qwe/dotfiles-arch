c4fe8c14e787079427551a5fe864d4bbb8321d12
Dallas Elliott <iop098321qwe@pm.me>
2025-11-13 12:02:48 -0700

    espanso install script

---
+++ .chezmoiscripts/run_onchange_espanso.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Install espanso-wayland
+if [[ ! yay -Qs espanso-wayland $> /dev/null]]; then
+  yay -S --noconfirm --needed espanso-wayland
+fi
--- .chezmoiscripts/run_onchange_install-packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Install espanso-wayland
-yay -S --noconfirm --needed espanso-wayland
