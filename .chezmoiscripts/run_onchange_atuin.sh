a04dada216f90bbbf2e4d3c586e049d26a8160e1
Dallas Elliott <iop098321qwe@pm.me>
2025-11-13 12:45:01 -0700

    commit

--- .chezmoiscripts/run_onchange_atuin.sh
+++ .chezmoiscripts/run_onchange_atuin.sh
@@ -2,7 +2,7 @@
 
 # Install Atuin
 if ! yay -Qs atuin &> /dev/null; then
-  yay -S --noconfirm --needed atuin
+  curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 else
   echo "atuin already installed."
 fi
