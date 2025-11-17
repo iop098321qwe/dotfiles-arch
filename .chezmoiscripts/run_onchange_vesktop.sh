a97e7762f86cc083ffd54179641da22c1cccfec5
Dallas Elliott <iop098321qwe@pm.me>
2025-11-16 18:54:55 -0700

    commit

--- .chezmoiscripts/run_onchange_vesktop.sh
+++ .chezmoiscripts/run_onchange_vesktop.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Install Vesktop
+if ! yay -Qs vesktop &> /dev/null; then
+  yay -S --noconfirm --needed vesktop-bin
+else
+  echo "vesktop already installed."
+fi
--- .chezmoiscripts/run_onchange_yazi.sh
+++ .chezmoiscripts/run_onchange_yazi.sh
@@ -2,7 +2,7 @@
 
 # Install Yazi
 if ! yay -Qs yazi &> /dev/null; then
-  yay -S --noconfirm --needed yazi-bin
+  yay -S --noconfirm --needed yazi
 else
   echo "yazi already installed."
 fi
