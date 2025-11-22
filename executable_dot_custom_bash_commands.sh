525972494b0ff9b3668ddce27b7af42ff43a8dfb
Dallas Elliott <iop098321qwe@pm.me>
2025-11-21 18:35:51 -0700

    Update .cbc_aliases.sh Update .custom_bash_commands.sh

--- executable_dot_custom_bash_commands.sh
+++ executable_dot_custom_bash_commands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERSION="v305.5.0"
+VERSION="v305.5.1"
 
 ###############################################################################
 # Charmbracelet Gum helpers (Catppuccin Mocha palette)
