44d2e01321542c03cacd687fa2a3fd014e639066
Dallas Elliott <iop098321qwe@pm.me>
2025-11-25 16:59:36 -0700

    Update .chezmoiscripts/omarchy_theme_hook.sh

--- .chezmoiscripts/run_once_omarchy_theme_hook.sh
+++ .chezmoiscripts/run_once_omarchy_theme_hook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Install install omarchy-theme-hook by imbypass
-curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+# curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
