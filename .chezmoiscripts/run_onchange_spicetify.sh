b2003667e76cd28a7185163d12a53ec3150f9be6
Dallas Elliott <iop098321qwe@pm.me>
2025-11-25 20:36:31 -0700

    Update .chezmoiscripts/spicetify.sh

--- .chezmoiscripts/run_onchange_spicetify.sh
+++ .chezmoiscripts/run_onchange_spicetify.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-# Install spicetify
-if ! yay -Qs spicetify &> /dev/null; then
-  yay -S --noconfirm --needed spicetify-cli
-  sudo chmod a+wr /opt/spotify
-  sudo chmod a+wr /opt/spotify/Apps -R
-  spicetify
-  spicetify backup apply enable-devtools
-  spicetify update
-else
-  echo "spicetify already installed."
-fi
+# Check if Spotify is installed
+if command -v spotify >/dev/null 2>&1; then
+  # Spotify found, now check if Spicetify is installed
+  if ! command -v spicetify >/dev/null 2>&1; then
+    yay -S --noconfirm --needed spicetify-cli
 
+    sudo chmod a+wr /opt/spotify
+    sudo chmod a+wr /opt/spotify/Apps -R
+
+    spicetify
+    spicetify backup apply enable-devtools
+    spicetify update
+  else
+    echo "spicetify already installed."
+  fi
+else
+  echo "Spotify is not installed. Skipping Spicetify setup."
+fi
