2ba14ee2253f2d47af762dd886742d8e0d88af7c
Dallas Elliott <iop098321qwe@pm.me>
2025-11-25 17:32:18 -0700

    chore: cleanup

--- .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
+++ .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # Install install omarchy-theme-hook by imbypass
-# if gum confirm "Install omarchy-theme-hook by imbypass?"; then
-#   curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
-# fi
-
 if ! command -v theme-hook-update >/dev/null 2>&1; then
   curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
 else
