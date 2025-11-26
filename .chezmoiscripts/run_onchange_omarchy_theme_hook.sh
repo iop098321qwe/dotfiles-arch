5f37f2185c63568543e4b02a4d11231844b51629
Dallas Elliott <iop098321qwe@pm.me>
2025-11-25 17:31:18 -0700

    Update .chezmoiscripts/omarchy_theme_hook.sh

--- .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
+++ .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
@@ -5,9 +5,8 @@
 #   curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
 # fi
 
-if ! yay -Qs theme-hook-update &> /dev/null; then
+if ! command -v theme-hook-update >/dev/null 2>&1; then
   curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
 else
   echo "omarchy-theme-hook already installed."
 fi
-
