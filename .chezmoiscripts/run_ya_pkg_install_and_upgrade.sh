b183d8cda331b5e6c30b5e4861d71585c99da6c7
Dallas Elliott <iop098321qwe@pm.me>
2025-11-26 18:59:56 -0700

    Update .chezmoiscripts/ya_pkg_install_and_upgrade.sh

--- .chezmoiscripts/run_ya_pkg_install_and_upgrade.sh
+++ .chezmoiscripts/run_ya_pkg_install_and_upgrade.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 # Upgrade ya packages
-if yay -Qs yazi &> /dev/null; then
-  ya pkg install && ya pkg upgrade
-else
-  echo "yazi is not installed."
-fi
+# if yay -Qs yazi &> /dev/null; then
+#   ya pkg install && ya pkg upgrade
+# else
+#   echo "yazi is not installed."
+# fi
 
+if gum confirm "Would you like to update yazi packages?": then
+  if yay -Qs yazi &> /dev/null; then
+    ya pkg install && ya pkg upgrade
+  else
+    echo "yazi is not installed."
+  fi
+else
+  echo "Skipping yazi packages update."
+fi
