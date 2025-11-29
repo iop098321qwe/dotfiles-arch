1a857ca6be86f3aa85702c195ad68931db69477a
Dallas Elliott <iop098321qwe@pm.me>
2025-11-28 17:33:14 -0700

    Update .chezmoiscripts/sponge.sh

--- .chezmoiscripts/run_onchange_sponge.sh
+++ .chezmoiscripts/run_onchange_sponge.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Install Sponge
 if ! yay -Qs sponge &> /dev/null; then
-  yay -S --noconfirm --needed sponge
+  yay -S --noconfirm --needed moreutils
 else
   echo "sponge already installed."
 fi
