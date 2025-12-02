98c39e3ebf1f5bbba31bec5d8d497659d06f5d35
Dallas Elliott <iop098321qwe@pm.me>
2025-12-01 22:59:02 -0700

    Update .chezmoiscripts/proton_vpn.sh

--- .chezmoiscripts/run_onchange_proton_vpn.sh
+++ .chezmoiscripts/run_onchange_proton_vpn.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Install Proton VPN
 if ! yay -Qs proton-vpn-gtk-app &> /dev/null; then
   yay -S --noconfirm --needed proton-vpn-gtk-app
 else
   echo "proton-vpn-gtk-app already installed."
+fi
+
+# Install networkmanager
+if ! yay -Qs networkmanager &> /dev/null; then
+  yay -S --noconfirm --needed networkmanager
+  yay -S --noconfirm --needed network-manager-applet
+  systemctl enable networkmanager.service
+  systemctl start networkmanager.service
+else
+  echo "networkmanager already installed."
 fi
