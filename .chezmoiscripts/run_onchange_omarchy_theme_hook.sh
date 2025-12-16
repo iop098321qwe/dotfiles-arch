180fd4f7599d68e2b779333ddd57330b43e65954
iop098321qwe <iop098321qwe@pm.me>
2025-12-16 10:15:15 -0700

    Update .chezmoiscripts/omarchy_theme_hook.sh Update .config/spicetify/config-xpui.ini Update .config/vesktop/themes/vencord.theme.css Update .config/solaar/config.yaml

--- .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
+++ .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Install install adw-gtk-theme
+if ! command -v adw-gtk-theme >/dev/null 2>&1; then
+  yay -S --noconfirm --needed adw-gtk-theme
+else
+  echo "adw-gtk-theme already installed."
+fi
+
 # Install install omarchy-theme-hook by imbypass
 if ! command -v theme-hook-update >/dev/null 2>&1; then
   curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
