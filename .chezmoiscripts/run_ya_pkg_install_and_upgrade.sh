d512fbceec5f9bb636445f03cc09e4874356113f
iop098321qwe <iop098321qwe@pm.me>
2025-12-19 13:51:37 -0700

    Update .chezmoiscripts/ya_pkg_install_and_upgrade.sh

--- .chezmoiscripts/run_ya_pkg_install_and_upgrade.sh
+++ .chezmoiscripts/run_ya_pkg_install_and_upgrade.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
+update=false
 # Upgrade ya packages
-if gum confirm "Would you like to update yazi packages?"; then
+
+yainstall() {
+  ya pkg install
+  ya pkg upgrade
+}
+
+checkyainstall() {
   if yay -Qs yazi &> /dev/null; then
-    ya pkg install && ya pkg upgrade
+   yainstall
   else
     echo "yazi is not installed."
   fi
+}
+
+if gum confirm "Would you like to update yazi packages?"; then
+  update=true
+else
+  update=false
+fi
+
+if [ $update == true ]; then
+  checkyainstall
 else
   echo "Skipping yazi packages update."
 fi
