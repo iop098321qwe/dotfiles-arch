18ca3673d7903bfd3d9bc89e4bad02658d3f0a37
iop098321qwe <iop098321qwe@pm.me>
2026-01-16 13:31:42 -0700

    Update .chezmoiscripts/proton_mail_bridge.sh

--- .chezmoiscripts/run_onchange_proton_mail_bridge.sh
+++ .chezmoiscripts/run_onchange_proton_mail_bridge.sh
@@ -3,6 +3,8 @@
 # Install Proton Mail Bridge
 if ! yay -Qs protonmail-bridge &> /dev/null; then
   yay -S --noconfirm --needed protonmail-bridge
+  systemctl --user enable protonmail-bridge
+  systemctl --user start protonmail-bridge   
 else
   echo "protonmail-bridge already installed."
 fi
