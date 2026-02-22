7f09ebfc059c1261d226ca9509efff93600934ca
iop098321qwe <iop098321qwe@pm.me>
2026-02-21 20:58:14 -0700

    Update .config/yazi/init.lua Update .config/yazi/keymap.toml Update .config/yazi/package.toml

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,19 +1,23 @@
 -- ~/.config/yazi/init.lua
--- set relative line numbers
+
+-- Relative Motions
 require("relative-motions"):setup({ show_numbers = "relative" })
 
--- set smart enter to open multiple lines
+-- Smart Enter
 require("smart-enter"):setup({
-	open_multi = true,
+  open_multi = true,
 })
 
 -- starship prompt
 require("starship"):setup({
-	-- Hide flags (such as filter, find and search). This is recommended for starship themes which
-	-- are intended to go across the entire width of the terminal.
-	hide_flags = false, -- Default: false
-	-- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
-	flags_after_prompt = true, -- Default: true
-	-- Custom starship configuration file to use
-	config_file = "~/.config/yazi_starship.toml", -- Default: nil
+  -- Hide flags (such as filter, find and search). This is recommended for starship themes which
+  -- are intended to go across the entire width of the terminal.
+  hide_flags = false,                           -- Default: false
+  -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+  flags_after_prompt = true,                    -- Default: true
+  -- Custom starship configuration file to use
+  config_file = "~/.config/yazi_starship.toml", -- Default: nil
 })
+
+-- Autosession
+require("autosession"):setup()
