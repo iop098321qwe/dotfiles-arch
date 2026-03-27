f31f43fb891c874d94f33aa74c7da6ca65c45225
iop098321qwe <iop098321qwe@pm.me>
2026-03-27 10:55:11 -0600

    Update .chezmoiscripts/intel_media_driver.sh

--- .chezmoiscripts/run_once_intel_media_driver.sh
+++ .chezmoiscripts/run_once_intel_media_driver.sh
@@ -2,3 +2,9 @@
 
 # Install Intel Media Driver
 yay -S --noconfirm --needed intel-media-driver
+
+if ! yay -Qs intel-media-driver &> /dev/null; then
+  yay -S --noconfirm --needed intel-media-driver
+else
+  echo "intel-media-driver already installed."
+fi
