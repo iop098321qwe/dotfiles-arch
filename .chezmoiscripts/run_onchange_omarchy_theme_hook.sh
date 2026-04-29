82e5ce8baa82c150342b9721bb21448339ac5d33
iop098321qwe <iop098321qwe@pm.me>
2026-04-29 10:20:41 -0600

    Update .chezmoiscripts/omarchy_theme_hook.sh

--- .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
+++ .chezmoiscripts/run_onchange_omarchy_theme_hook.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # Install install omarchy-theme-hook by imbypass
-if ! command -v theme-hook-update >/dev/null 2>&1; then
-  curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+if gum confirm "Install omarchy-theme-hook?"; then
+  if ! command -v theme-hook-update >/dev/null 2>&1; then
+    curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+  else
+    echo "omarchy-theme-hook already installed."
+  fi
 else
-  echo "omarchy-theme-hook already installed."
+  echo "Skipping omarchy-theme-hook install."
 fi
