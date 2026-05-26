ec8f9c9692f0541c3fc66e8bcc1fc910f2211b20
iop098321qwe <iop098321qwe@pm.me>
2026-05-26 16:28:14 -0600

    Update .chezmoiscripts/atuin.sh

--- .chezmoiscripts/run_onchange_atuin.sh
+++ .chezmoiscripts/run_onchange_atuin.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Atuin
-if ! yay -Qs atuin &> /dev/null; then
-  curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-else
-  echo "atuin already installed."
-fi
+omarchy pkg add atuin
 
-if ! yay -Qs blesh &> /dev/null; then
-  yay -S --noconfirm --needed blesh-git
-else
-  echo "blesh already installed."
-fi
+# blesh-git
+omarchy pkg aur add blesh-git
