d669c11d0cc75bcfe22f9cfaa6b025f3ac873e68
iop098321qwe <iop098321qwe@pm.me>
2026-06-19 15:31:45 -0600

    Update .config/cbc/packages.toml Update .config/harper-ls/dictionary.txt Update .config/tmux/tmux.conf Update .config/yazi/package.toml Update .custom_bash_commands.sh

--- executable_dot_custom_bash_commands.sh
+++ executable_dot_custom_bash_commands.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CBC_VERSION="v3.3.0"
+CBC_VERSION="v3.4.0"
 
 ################################################################################
 # CUSTOM BASH COMMANDS (by iop098321qwe)
