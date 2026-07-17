9a9c9f560921d7c5d501e0825e3edfc69b2877e0
iop098321qwe <iop098321qwe@pm.me>
2026-07-16 18:08:20 -0600

    Update .chezmoiscripts/spicetify.sh

--- .chezmoiscripts/run_onchange_spicetify.sh
+++ .chezmoiscripts/run_onchange_spicetify.sh
@@ -4,7 +4,7 @@
 if command -v spotify >/dev/null 2>&1; then
   # Spotify found, now check if Spicetify is installed
   if ! command -v spicetify >/dev/null 2>&1; then
-    yay -S --noconfirm --needed spicetify-cli
+    omarchy pkg aur add spicetify-cli
 
     sudo chmod a+wr /opt/spotify
     sudo chmod a+wr /opt/spotify/Apps -R
