4d460d134ab3a07ab31bc4bece9588d580f9a03c
iop098321qwe <iop098321qwe@pm.me>
2026-08-07 11:56:10 -0600

    Update .chezmoiscripts/diffnav.sh

--- .chezmoiscripts/run_onchange_diffnav.sh
+++ .chezmoiscripts/run_onchange_diffnav.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
 # Install diffnav
-if ! yay -Qs diffnav &> /dev/null; then
-  yay -S --noconfirm --needed diffnav-bin
-else
-  echo "diffnav already installed."
-fi
-
+omarchy pkg add diffnav-bin
