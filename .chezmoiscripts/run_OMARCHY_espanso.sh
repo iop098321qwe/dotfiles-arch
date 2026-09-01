942ea2bf13e47495823ea54254964f68b8a7b2b5
Dallas Elliott <iop098321qwe@pm.me>
2026-09-01 00:28:59 -0800

    fix(espanso): make installation script idempotent

--- .chezmoiscripts/run_OMARCHY_espanso.sh
+++ .chezmoiscripts/run_OMARCHY_espanso.sh
@@ -4,10 +4,7 @@
 omarchy pkg aur add espanso-wayland
 
 # Ensure the Espanso service is registered.
-if ! systemctl --user cat espanso.service &> /dev/null; then
-  echo "Registering Espanso service..."
-  espanso service register
-fi
+espanso service register
 
 # Ensure Espanso is running.
 systemctl --user is-active --quiet espanso.service || espanso start
